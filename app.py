f43fc510b3d5269a3122eb99de7a2ed6d7779928
Christos Lazaridis <clazaridis7@gmail.com>
2024-06-13 00:26:20 +0300

    Made the files used for code implementations and wrote TODO's

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run()
+
+# TODO: get the message, decode it using the linear code, decompress it using the fano-shanon algorithm
+# TODO: and print the result
